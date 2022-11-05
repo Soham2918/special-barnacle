2f9a9edefc9a3ce571071cabe6515f36d4c2b2ba
Soham2918 <joshipg1@gmail.com>
2022-11-05 20:46:49 +0530

    added first

---
+++ main.js
@@ -0,0 +1,19 @@
+document.getElementById("one").innerHTML='this is a h1 one';
+
+document.getElementById("three").innerHTML='This is h2';
+
+num1=9;
+num2=9;
+mult=num1*num2;
+document.write('<br></br> Multiply is :',mult)
+
+number1=prompt('Enter Number 1 :');
+number2=prompt('Enter Number 2 :');
+num3=number1*number2;
+alert('output is::'+num3)
+
+val1=document.getElementById('first').value;
+val2=document.getElementById('second').value;
+val=val1*val2;
+document.write("<br> <br> Value is :"+val)
+
